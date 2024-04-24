aea23f76ae712b945a158092c674ea4c1ea43ce0
Allan Cassio <allan.cassio1@gmail.com>
2024-04-24 05:17:31 -0300

    create hash from password

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,41 @@
+import { ConflictException } from '@nestjs/common'
+import { Body, Controller, HttpCode, Post } from '@nestjs/common'
+import { PrismaService } from 'src/prisma/pisma.services'
+import { hash } from 'bcryptjs'
+import { z } from 'zod'
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+})
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
+
+@Controller('/accounts')
+export class CreateAccountController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  @HttpCode(201)
+  async handle(@Body() body: CreateAccountBodySchema) {
+    const { email, name, password } = createAccountBodySchema.parse(body)
+
+    const userAlreadyExists = await this.prisma.user.findUnique({
+      where: { email },
+    })
+
+    if (userAlreadyExists)
+      throw new ConflictException('user already exists with email equal')
+
+    const password_hash = await hash(password, 8)
+
+    await this.prisma.user.create({
+      data: {
+        email,
+        name,
+        password: password_hash,
+      },
+    })
+  }
+}
