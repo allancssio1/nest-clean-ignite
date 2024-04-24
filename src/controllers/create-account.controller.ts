634bc7588ad61b585a37ef28a07fe9605ec614d6
Allan Cassio <allan.cassio1@gmail.com>
2024-04-24 15:17:18 -0300

    implement pipe from errors validation

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -1,8 +1,9 @@
-import { ConflictException } from '@nestjs/common'
+import { ConflictException, UsePipes } from '@nestjs/common'
 import { Body, Controller, HttpCode, Post } from '@nestjs/common'
 import { PrismaService } from 'src/prisma/pisma.services'
 import { hash } from 'bcryptjs'
 import { z } from 'zod'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
 
 const createAccountBodySchema = z.object({
   name: z.string(),
@@ -18,6 +19,7 @@ export class CreateAccountController {
 
   @Post()
   @HttpCode(201)
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
   async handle(@Body() body: CreateAccountBodySchema) {
     const { email, name, password } = createAccountBodySchema.parse(body)
 
---
+++ src/pipes/zod-validation-pipe.ts
@@ -0,0 +1,26 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common'
+import { ZodError, ZodObject } from 'zod'
+import { fromZodError } from 'zod-validation-error'
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schme: ZodObject<any>) {}
+
+  transform(value: any, metadata: ArgumentMetadata) {
+    try {
+      this.schme.parse(value)
+    } catch (error) {
+      if (error instanceof ZodError)
+        throw new BadRequestException({
+          errors: fromZodError(error),
+          message: 'Validation failed',
+          statusCOde: 400,
+        })
+    }
+
+    return value
+  }
+}
