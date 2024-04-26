edf3dc4aa35c800005286dab4affb775ee0ac229
Allan Cassio <allan.cassio1@gmail.com>
2024-04-26 16:03:30 -0300

    create authenticate to jwt public and private key

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { PrismaService } from './prisma/pisma.services'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { ConfigModule } from '@nestjs/config'
 import { envShcema } from './env'
+import { AuthenticateController } from './controllers/authenticate.controller'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
   imports: [
@@ -10,8 +12,9 @@ import { envShcema } from './env'
       validate: (env) => envShcema.parse(env),
       isGlobal: true,
     }),
+    AuthModule,
   ],
-  controllers: [CreateAccountController],
+  controllers: [CreateAccountController, AuthenticateController],
   providers: [PrismaService],
 })
 export class AppModule {}
