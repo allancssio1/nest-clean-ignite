7413b9a98da50bff5b049ded4b2f7214fe832f63
Allan Cassio <allan.cassio1@gmail.com>
2024-05-14 05:48:49 -0300

    config to tests for tests isolated e2e

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -5,9 +5,10 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   test: {
     include: ['**/*.e2e-spec.ts'],
-    exclude: [...configDefaults.exclude, '**/data/pg/**'],
-    globals: true,
+    exclude: [...configDefaults.exclude, '**/data/pg/**'], // resolove error require config to data/pg on tests end-to-end
+    globals: true, // makes it unnecessary to import test functions into the test file.
     root: './',
+    setupFiles: ['./test/setup-e2e.ts'], // config run this files before runs to tests.
   },
   plugins: [
     tsConfigPaths(),
